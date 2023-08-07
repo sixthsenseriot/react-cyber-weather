da20bd71458e9ab105d6d0be32a22920ddbd8e9c
Khanh <knh.nguyen2002@gmail.com>
2023-08-07 11:35:22 -0500

    Added search weather function

--- src/Weather.js
+++ src/Weather.js
@@ -5,6 +5,7 @@ import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -18,18 +19,35 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "bc2cd97eaa209e7d22d8f3c84081655f";
+    let units = "imperial";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(city);
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
         <h1 className="text-center">WEATHER FORECAST</h1>
         <div className="container weather-app-container">
           <div className="search-container d-flex justify-content-center pt-3">
-            <form className="search-bar">
+            <form className="search-bar" onSubmit={handleSubmit}>
               <input
                 type="text"
                 placeholder="Where to?"
                 className="city-input mr-1"
                 autoComplete="off"
+                onChange={handleCityChange}
               />
               <button className="mr-1" type="submit">
                 SEARCH
@@ -59,11 +77,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "bc2cd97eaa209e7d22d8f3c84081655f";
-    let units = "imperial";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return <h1>Loading...</h1>;
   }
 }
